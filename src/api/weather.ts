75f3e6f462b38c0453819b383db0d73ad26bcac9
kobayashi-m42 <m42kobayashi@gmail.com>
2018-09-20 14:33:46 +0900

    :green_heart: #4 テストケースを追加

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -3,7 +3,7 @@ import axios, { AxiosResponse } from "axios";
 export const WeatherAPI = {
   fetchWeather: async (): Promise<any> => {
     const response: AxiosResponse<any> = await axios
-      .get("http://127.0.0.1:8000/api/weathe")
+      .get("http://127.0.0.1:8000/api/weather")
       .catch(error => Promise.reject(error));
 
     return response;
