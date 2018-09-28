b237134d33b856c11c55dd78aa9bd577003d2da9
kobayashi-m42 <m42kobayashi@gmail.com>
2018-09-28 18:03:27 +0900

    :white_check_mark: #17 テストを追加

--- tests/unit/Login.spec.ts
+++ tests/unit/Login.spec.ts
@@ -1,11 +1,24 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 import Login from "../../src/components/Login.vue";
+import { QiitaModule } from "@/store/modules/Qiita";
+
 import { LoginState } from "@/types/login";
+import VueRouter from "vue-router";
 
 const localVue = createLocalVue();
 
 localVue.use(Vuex);
+localVue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/login",
+    component: Login
+  }
+];
+
+const router = new VueRouter({ routes });
 
 describe("Login.vue", () => {
   let store: any;
@@ -15,24 +28,27 @@ describe("Login.vue", () => {
   it('calls store action "login" when button is clicked', () => {
     state = {
       authorizationCode: "",
-      accessToken: ""
+      accessToken: "",
+      permanentId: ""
     };
 
     actions = {
-      login: jest.fn()
+      login: jest.fn(),
+      issueAccessToken: jest.fn()
     };
 
     store = new Vuex.Store({
       modules: {
-        LoginModule: {
+        QiitaModule: {
           namespaced: true,
           state,
-          actions
+          actions,
+          getters: QiitaModule.getters
         }
       }
     });
 
-    const wrapper = shallowMount(Login, { store, localVue });
+    const wrapper = shallowMount(Login, { store, localVue, router });
     const button = wrapper.find("button");
 
     button.trigger("click");
