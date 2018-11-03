5d495b57552665d1cb57f044874246f63664bad0
kobayashi-m42 <m42kobayashi@gmail.com>
2018-11-03 20:30:49 +0900

    :sparkles: #65 Headerコンポーネントを作成

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <nav class="navbar has-shadow">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="http://localhost:8080"><p class="is-size-5 has-text-black">Qiita Stocker</p></a>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <button class="button is-small">ボタン</button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue {}
+</script>
