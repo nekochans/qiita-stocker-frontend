d6ba32fed3f749269a4fd0379fb53e562c29257c
kobayashi-m42 <m42kobayashi@gmail.com>
2018-11-03 13:37:46 +0900

    :sparkles: #49 アカウント作成画面にBulmaのスタイルを導入

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
