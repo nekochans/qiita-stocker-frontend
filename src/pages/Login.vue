d1587df59d9b8632d166a6aa233aa1d9d1e06c18
kobayashi-m42 <m42kobayashi@gmail.com>
2018-11-05 16:42:12 +0900

    :sparkles: #77 ログインページのスタイリング

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,19 +1,41 @@
 <template>
-  <div>
-    <h1>ログイン</h1>
-    <button @click="login">Qiitaアカウントでログイン</button>
-  </div>
+  <section class="hero is-fullheight">
+    <Header />
+    <main>
+      <div class="container has-text-centered">
+        <h1 class="title">ログイン</h1>
+        <button class="button" @click="login">Qiitaアカウントでログイン</button>
+        <h2 class="is-size-7 login-guide">
+          アカウントを持っていない場合は<a href="/signup">新規登録</a>から
+        </h2>
+      </div>
+    </main>
+    <Footer />
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { Action, namespace } from "vuex-class";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
 const QiitaAction = namespace("QiitaModule", Action);
 
-@Component
+@Component({
+  components: {
+    Header,
+    Footer
+  }
+})
 export default class Login extends Vue {
   @QiitaAction
   login!: () => void;
 }
 </script>
+
+<style scoped>
+.login-guide {
+  padding-top: 1rem;
+}
+</style>
