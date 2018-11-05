a5be2746cba3a12dfda99c664b9ca7bfe46f14be
kobayashi-m42 <m42kobayashi@gmail.com>
2018-11-05 14:56:32 +0900

    :sparkles: #68 エラーページのスタイルを修正

--- src/pages/Error.vue
+++ src/pages/Error.vue
@@ -1,18 +1,36 @@
 <template>
-  <div>
-    <h1>Error</h1>
-    <p>
-      {{ errorMessage }}
-    </p>
-  </div>
+  <section class="hero is-fullheight">
+    <Header />
+    <main class="container has-text-centered">
+      <h1 class="title">Error</h1>
+      <h2 class="subtitle">
+        {{ errorMessage }}
+      </h2>
+      <a href="/">TOPページへ</a>
+    </main>
+    <Footer />
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 
-@Component
+@Component({
+  components: {
+    Header,
+    Footer
+  }
+})
 export default class Error extends Vue {
   @Prop()
   errorMessage!: string;
 }
 </script>
+
+<style scoped>
+.subtitle {
+  padding-top: 1rem;
+}
+</style>
