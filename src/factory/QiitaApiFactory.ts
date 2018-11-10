cf6d47ec62cf301f218652c5246eb8a987b48082
kobayashi-m42 <m42kobayashi@gmail.com>
2018-11-11 04:39:18 +0900

    :recycle: #61 Factoryパターンを導入

---
+++ src/factory/QiitaApiFactory.ts
@@ -0,0 +1,9 @@
+import QiitaApi from "@/infrastructure/api/qiitaApi";
+import { IQiitaApi } from "@/domain/Qiita";
+
+export default class QiitaApiFactory {
+  static create(): IQiitaApi {
+    const qiitaApi = new QiitaApi();
+    return qiitaApi;
+  }
+}
