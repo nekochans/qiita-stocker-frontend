1c3254dae0317bd46c43c63b0d0c2d93f62368ae
kobayashi-m42 <m42kobayashi@gmail.com>
2018-11-12 20:33:32 +0900

    :wrench: #92 不要な設定を削除

--- jest.config.js
+++ jest.config.js
@@ -13,11 +13,5 @@ module.exports = {
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
   ],
-  testURL: "http://localhost/",
-  globals: {
-    "ts-jest": {
-      pathRegex: /\.(spec|test)\.ts$/,
-      diagnostics: false
-    }
-  }
+  testURL: "http://localhost/"
 };
