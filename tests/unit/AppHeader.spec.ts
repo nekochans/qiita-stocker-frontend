a2040c4903212af23f72d2d6cf31e3e7a2aaa506
kobayashi-m42 <m42kobayashi@gmail.com>
2018-12-17 20:28:03 +0900

    :white_check_mark: #123 AppHeaderのテストケースを追加

---
+++ tests/unit/AppHeader.spec.ts
@@ -0,0 +1,65 @@
+import { shallowMount, createLocalVue, config } from "@vue/test-utils";
+import Vuex from "vuex";
+import { QiitaModule } from "@/store/modules/qiita";
+import AppHeader from "@/components/AppHeader.vue";
+import { IQiitaState } from "@/types/qiita";
+import VueRouter from "vue-router";
+
+config.logModifiedComponents = false;
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+localVue.use(VueRouter);
+
+const router = new VueRouter();
+
+describe("AppHeader.vue", () => {
+  let store: any;
+  let state: IQiitaState;
+
+  state = {
+    authorizationCode: "",
+    accessToken: "",
+    qiitaAccountId: "",
+    permanentId: "",
+    sessionId: "",
+    categories: []
+  };
+
+  it("renders login", () => {
+    store = new Vuex.Store({
+      modules: {
+        QiitaModule: {
+          namespaced: true,
+          state,
+          actions: {},
+          getters: QiitaModule.getters
+        }
+      }
+    });
+
+    const wrapper = shallowMount(AppHeader, { store, localVue, router });
+    const navbarItem = wrapper.findAll("a").at(2);
+    expect(navbarItem.text()).toBe("ログイン");
+  });
+
+  it("renders logout", () => {
+    state.sessionId = "d690e4de-0a4e-4f14-a5c5-f4303fbd8a08";
+
+    store = new Vuex.Store({
+      modules: {
+        QiitaModule: {
+          namespaced: true,
+          state,
+          actions: {},
+          getters: QiitaModule.getters
+        }
+      }
+    });
+
+    const wrapper = shallowMount(AppHeader, { store, localVue, router });
+    const navbarItem = wrapper.findAll("a").at(4);
+    expect(navbarItem.text()).toBe("ログアウト");
+  });
+});
