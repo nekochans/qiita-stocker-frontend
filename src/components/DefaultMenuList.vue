2d9d7c0a850befcbd494db810cbe949bc1587beb
kobayashi-m42 <m42kobayashi@gmail.com>
2018-12-19 22:50:00 +0900

    :sparkles: #117 デフォルトメニューのコンポーネントを作成

---
+++ src/components/DefaultMenuList.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <ul class="menu-list">
+      <li><a class="is-active">全てのストック</a></li>
+      <li><a>未分類</a></li>
+    </ul>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class DefaultMenuList extends Vue {}
+</script>
