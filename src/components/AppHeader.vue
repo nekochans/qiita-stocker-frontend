53e4399802a942f6b76992ee776f8dbd21bdb774
kobayashi-m42 <m42kobayashi@gmail.com>
2018-12-24 13:45:48 +0900

    :recycle: #138 ヘッダーの文言を修正

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -23,7 +23,7 @@
           </div>
           <div class="navbar-end" v-else>
             <div class="navbar-item has-dropdown is-hoverable">
-              <a class="navbar-link">アカウント</a>
+              <a class="navbar-link">メニュー</a>
               <div class="navbar-dropdown is-right">
                 <a class="navbar-item" href="/stocks/all">ストック一覧</a>
                 <a class="navbar-item" @click="logout">ログアウト</a>
