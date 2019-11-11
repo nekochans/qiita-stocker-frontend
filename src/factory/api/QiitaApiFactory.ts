96ddc963711d263d1c05a4d9ab0b212a1fa445e2
kobayashi-m42 <m42kobayashi@gmail.com>
2019-11-11 16:20:31 +0900

    :fire: Nuxt.jsへ移行するため一時的に全てのファイルを削除

--- src/factory/api/QiitaApiFactory.ts
+++
@@ -1,9 +0,0 @@
-import QiitaApi from "../../infrastructure/api/qiitaApi";
-import { IQiitaApi } from "@/domain/qiita";
-
-export default class QiitaApiFactory {
-  static create(): IQiitaApi {
-    const qiitaApi = new QiitaApi();
-    return qiitaApi;
-  }
-}
--- src/factory/repository/LocalStorageFactory.ts
+++
@@ -1,9 +0,0 @@
-import { IQiitaStockerSessionStorage } from "../../domain/qiita";
-import LocalStorage from "@/infrastructure/repository/localStorage";
-
-export default class LocalStorageFactory {
-  static create(): IQiitaStockerSessionStorage {
-    const localStorage = new LocalStorage();
-    return localStorage;
-  }
-}
