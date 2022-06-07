23006687381a5fe8354b6684e71ad002c901c839
keitakn <keita.koga.work@gmail.com>
2022-06-07 16:03:34 +0900

    :art: 長期メンテナンス状態とする為、全てのルーティングを /maintenance にするように変更

---
+++ app/components/pages/Maintenance.vue
@@ -0,0 +1,24 @@
+<template>
+  <main>
+    <div class="container has-text-centered">
+      <h1 class="title">長期メンテナンス中です🐱</h1>
+      <h2 class="subtitle">{{ message }}</h2>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class extends Vue {
+  @Prop()
+  message!: string
+}
+</script>
+
+<style scoped>
+.subtitle {
+  padding-top: 1rem;
+}
+</style>
--- app/middleware/redirectMiddleware.ts
+++ app/middleware/redirectMiddleware.ts
@@ -1,28 +1,7 @@
 import { Context, Middleware } from '@nuxt/types'
 
-const redirectMiddleware: Middleware = ({
-  store,
-  redirect,
-  route
-}: Context) => {
-  const notRequiredAuthorization = [
-    '/',
-    '/signup',
-    '/privacy',
-    '/terms',
-    '/cancel/complete',
-    '/error'
-  ]
-
-  if (notRequiredAuthorization.includes(route.path)) return
-
-  if (store.getters['qiita/isLoggedIn'] && route.path === '/login') {
-    return redirect('/stocks/all')
-  }
-
-  if (!store.getters['qiita/isLoggedIn'] && route.path !== '/login') {
-    return redirect('/')
-  }
+const redirectMiddleware: Middleware = ({ redirect }: Context) => {
+  return redirect('/maintenance')
 }
 
 export default redirectMiddleware
